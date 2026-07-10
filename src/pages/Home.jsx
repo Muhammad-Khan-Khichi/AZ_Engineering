bf348abf04464688b28ad307a0d8944b1183c534
Muhammad Khan <muhammadkhan08561@gmail.com>
2026-07-11 01:29:21 +0500

    adding home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,25 @@
+import Hero from '../components/sections/Hero'
+import About from '../components/sections/About'
+import Leadership from '../components/sections/Leadership'
+import Services from '../components/sections/Services'
+import Projects from '../components/sections/Projects'
+import MissionVision from '../components/sections/MissionVision'
+import Equipment from '../components/sections/Equipment'
+import Contact from '../components/sections/Contact'
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <About />
+      <Leadership />
+      <Services />
+      <Projects />
+      <MissionVision />
+      <Equipment />
+      <Contact />
+    </>
+  )
+}
+
+export default Home

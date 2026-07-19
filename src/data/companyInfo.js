755810caf6728bd1144c5520387f4b1b15de4319
Muhammad Khan <muhammadkhan08561@gmail.com>
2026-07-19 23:03:32 +0500

    updating company info

--- src/data/companyInfo.js
+++ src/data/companyInfo.js
@@ -3,8 +3,8 @@ export const companyInfo = {
   tagline: "Engineering Excellence Since 1999",
   established: 1999,
   experience: "24+ Years",
-  email: "shaukat@0973@gmail.com",
-  phones: ["0307- 0409362 ", " 0334-4387164"],
+  email: "azengineering.official@gmail.com",
+  phones: ["0307-0409362 ", " 0334-4387164"],
   address:
     "Bank Stop Attari Mor, Hakim Chowk, 17 km, Ferozepur Road, Lahore, Pakistan",
   hours: "Monday - Saturday, 9:00 AM - 6:00 PM",

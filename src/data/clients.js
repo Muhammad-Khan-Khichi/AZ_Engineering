50dba67662e3645a13eb15c08e2033ea289bd9ac
Muhammad Khan <muhammadkhan08561@gmail.com>
2026-08-21 23:21:09 +0500

    adding client file

---
+++ src/data/clients.js
@@ -0,0 +1,34 @@
+﻿const clients = [
+  {
+    id: 1,
+    name: 'Nishat Mills LTD',
+    logo: '/images/clients/nishat-mills.png',
+  },
+  {
+    id: 2,
+    name: 'Al Hadi Textile Pvt. Ltd.',
+    logo: '/images/clients/al-hadi-textile.png',
+  },
+  {
+    id: 3,
+    name: 'Sapphire',
+    logo: '/images/clients/sapphire.png',
+  },
+  {
+    id: 4,
+    name: 'Style Textile',
+    logo: '/images/clients/style-textile.png',
+  },
+  {
+    id: 5,
+    name: 'Ihsan Pakistan',
+    logo: '/images/clients/ihsan-pakistan.png',
+  },
+  {
+    id: 6,
+    name: 'ShafiTexcel',
+    logo: '/images/clients/shafi-texcel.png',
+  },
+]
+
+export default clients
